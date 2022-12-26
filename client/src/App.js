c367bdd13e0f841a9a02165e8a29462030561a4d
user <ujjalroy7862@gmail.com>
2022-12-27 01:03:53 +0600

    create my own chat gpt version

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+      <section className="chatbox">
+        
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
